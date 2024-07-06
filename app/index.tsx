43a39dc35ddbb99dabbeae19c7c97ff66b4903a4
MotuekaHighSchool <williamdk.101@gmail.com>
2024-07-07 02:57:43 +1200

    Added font capability

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontFamily: "Inter-Bold" }}>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
